b456bc3af561af515eb3391f99eace8ce99bd513
eee <erolkaanbostan@Erols-MacBook-Pro-2.local>
2023-04-30 13:42:09 -0700

    third

--- my_blog/new_blog/models.py
+++ my_blog/new_blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+class Post(models.Model):
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    content = models.TextField(MinLengthValidator(30))
+    importance = models.PositiveSmallIntegerField()
+    github = models.URLField()
+    explanation = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
+
+        
